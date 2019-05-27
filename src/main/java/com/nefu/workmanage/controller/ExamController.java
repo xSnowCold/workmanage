1d992f25912e28d72d7cade0c9c59ecd508a171f
snowCold <1209204477@qq.com>
2019-05-27 22:38:30 +0800

    2019.5.27 22:37 按状态查询监考列表、添加、删除  功能

--- src/main/java/com/nefu/workmanage/controller/ExamController.java
+++ src/main/java/com/nefu/workmanage/controller/ExamController.java
@@ -1,11 +1,10 @@
 package com.nefu.workmanage.controller;
 
+import com.nefu.workmanage.entity.Exam;
 import com.nefu.workmanage.service.ExamService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -16,8 +15,29 @@ public class ExamController {
     @Autowired
     private ExamService examService;
 
+    //获取全部监考信息
     @GetMapping("/exams")
     public Map getAllExam(){
         return Map.of("exams",examService.findAllExam());
     }
+
+    //根据状态获取监考信息
+    @GetMapping("/exams/{status}")
+    public Map getExamsByStatus(@PathVariable Exam.stat status){
+        return Map.of(""+status+"",examService.findExamStatus(status));
+    }
+
+    //添加监考信息
+    @PostMapping("/exams")
+    public Map addAExam(@RequestBody Exam exam){
+       return Map.of("反馈",examService.addExam(exam));
+    }
+
+    //删除教考信息
+    @DeleteMapping("/exams/{id}")
+    public Map deleteExam(@PathVariable int id){
+        examService.deleteExam(id);
+        return Map.of("删除",id);
+    }
+
 }
