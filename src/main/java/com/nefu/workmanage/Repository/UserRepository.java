f687cfdcfe3114373387f156a96c386ab1c09668
snowCold <1209204477@qq.com>
2019-05-28 22:33:06 +0800

    2019.5.28 22:31 登录功能，密码加密，当前用户的id和role加密保存至请求头

--- src/main/java/com/nefu/workmanage/Repository/UserRepository.java
+++ src/main/java/com/nefu/workmanage/Repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.nefu.workmanage.Repository;
 
 import com.nefu.workmanage.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+    @Query("SELECT u FROM User u WHERE u.account=:account")
+    User findByAccount(@Param("account") String account);
 }
--- src/main/java/com/nefu/workmanage/service/UserService.java
+++ src/main/java/com/nefu/workmanage/service/UserService.java
@@ -3,6 +3,7 @@ package com.nefu.workmanage.service;
 import com.nefu.workmanage.Repository.UserRepository;
 import com.nefu.workmanage.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +14,12 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     //添加用户
     public User addUser(User user){
+        String password = passwordEncoder.encode(user.getPassword());
+        user.setPassword(password);
         return userRepository.save(user);
        // return userRepository.refresh(user);
     }
@@ -25,6 +29,9 @@ public class UserService {
     }
     public List<User> findUser(){
         return userRepository.findAll();
+    }
+    public User findByAccount(String account){
+        return userRepository.findByAccount(account);
     }
     //删除用户
     public void deleteUser(int id){
